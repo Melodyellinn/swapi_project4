740759d99b156d02c40c1b2080f2253147e31a87
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2021-08-06 15:33:17 +0200

    swapi modif

--- swapi.py
+++ swapi.py
@@ -70,3 +70,60 @@ for i in range (2,n+1):
     except:
         print(f"Cette entrée numéro {i} n'existe pas sur l'API")
 #starships_df
+
+#Youcef
+
+#import json
+uri = "https://swapi.dev/api/vehicles/"
+
+def fetch_vehicles(n_vehicles):
+    uri = 'https://swapi.dev/api/vehicles/'
+    url = f'{uri}/{str(n_vehicles)}'
+    r = requests.get(url)
+    data = r.json()
+    return data
+#response = requests.get("https://swapi.dev/api/vehicles/")
+#datap=response.json()
+#datap
+
+def datasetvehicles(vehicles_list, vehicles_df, n_vehicles):
+    vehicles_df.loc[n_vehicles,'name'] = vehicles_list['name']
+    vehicles_df.loc[n_vehicles,'model'] = vehicles_list['model']
+    vehicles_df.loc[n_vehicles,'manufacturer'] = vehicles_list['manufacturer']
+    vehicles_df.loc[n_vehicles,'cost_in_credits'] = vehicles_list['cost_in_credits']
+    vehicles_df.loc[n_vehicles,'length'] = vehicles_list['length']
+    vehicles_df.loc[n_vehicles,'max_atmosphering_speed'] = vehicles_list['max_atmosphering_speed']
+    vehicles_df.loc[n_vehicles,'crew'] = vehicles_list['crew']
+    vehicles_df.loc[n_vehicles,'passengers'] = vehicles_list['passengers']
+    vehicles_df.loc[n_vehicles,'cargo_capacity'] = vehicles_list['cargo_capacity']
+    vehicles_df.loc[n_vehicles,'consumables'] = vehicles_list['consumables']
+    vehicles_df.loc[n_vehicles,'vehicle_class'] = vehicles_list['vehicle_class']
+    return vehicles_df
+#vehicles = pd.DataFrame(datap)
+#vehicles
+
+columns = ['name', 'model', 'manufacturer', 'cost_in_credits', 'length', 'max_atmosphering_speed', 'crew', 'passengers', 'cargo_capacity', 'consumables', 'vehicles_class']
+vehicles_df = pd.DataFrame(columns=columns)
+
+n = 39
+
+for i in range (1,n+1):
+    vehicles_lists = fetch_vehicles(i)
+    vehicles_df = datasetvehicles(vehicles_list, vehicles_df,i)
+planet_df
+#vehicles_2 = vehicles.drop(["next", "previous", "count"], axis=1)
+
+vehicles_2
+
+
+results
+0	{'name': 'Sand Crawler', 'model': 'Digger Craw...
+1	{'name': 'T-16 skyhopper', 'model': 'T-16 skyh...
+2	{'name': 'X-34 landspeeder', 'model': 'X-34 la...
+3	{'name': 'TIE/LN starfighter', 'model': 'Twin ...
+4	{'name': 'Snowspeeder', 'model': 't-47 airspee...
+5	{'name': 'TIE bomber', 'model': 'TIE/sa bomber...
+6	{'name': 'AT-AT', 'model': 'All Terrain Armore...
+7	{'name': 'AT-ST', 'model': 'All Terrain Scout ...
+8	{'name': 'Storm IV Twin-Pod cloud car', 'model...
+9	{'name': 'Sail barge', 'model': 'Modified Luxu...
